d78e34a6a76a75157ed69701134e7245b5c9feb7
squirt11e <coloradobites@gmail.com>
2023-05-15 13:27:28 -0600

    remove extra code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
