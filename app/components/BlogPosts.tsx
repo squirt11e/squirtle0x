a83e3fd5faaa07713fc94d41938010daa415a951
squirt11e <coloradobites@gmail.com>
2023-05-16 00:14:12 -0600

    wip

--- app/components/BlogPosts.tsx
+++ app/components/BlogPosts.tsx
@@ -4,9 +4,9 @@ export default function BlogPosts() {
   return (
     <section className="mb-8 flex flex-col">
       <h3 className="mb-4 text-2xl font-semibold text-green">Mirror</h3>
-      
+
       <div className="flex flex-row flex-wrap gap-4">
-        {tempPosts.map((post) => (
+        {tempPosts.map(post => (
           <div key={post} className="flex flex-col">
             <h4 className="mb-2 text-xl font-semibold text-green">{post}</h4>
             <div className="w-24 h-24 bg-purple rounded-lg"></div>
@@ -14,5 +14,5 @@ export default function BlogPosts() {
         ))}
       </div>
     </section>
-  )
+  );
 }
