3d95fe8bebca4f19dc745d96b1db30e877a8e1f2
squirt11e <coloradobites@gmail.com>
2023-05-17 02:12:48 -0600

    add 404 and 500 pages

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-semibold text-green">404</h1>
+      <h2 className="text-2xl font-semibold text-green">Page not found</h2>
+    </div>
+  );
+}
---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+export default function ErrorFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-semibold text-green">Error</h1>
+      <h2 className="text-2xl font-semibold text-green">Please try again</h2>
+    </div>
+  );
+}
