afbb6237461a7d429453fd9cb4056d2346f67324
squirt11e <coloradobites@gmail.com>
2023-05-17 04:24:57 -0600

    use rainbow

--- app/components/ConnectWallet.tsx
+++ app/components/ConnectWallet.tsx
@@ -1,30 +1,17 @@
-import { useAccount, useConnect, useDisconnect, useEnsName } from 'wagmi';
-import { InjectedConnector } from 'wagmi/connectors/injected';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const ConnectWallet = () => {
-  // Get account address and connection status
-  const { address, isConnected } = useAccount();
-  const { connect } = useConnect({
-    connector: new InjectedConnector(),
-  });
-
-  // Disconnect wallet
-  const { disconnect } = useDisconnect();
-
-  // Get ENS name
-  const { data, isError, isLoading } = useEnsName({
-    address: address,
-  });
-  // Show ENS name if it exists, otherwise show address
-  const ensName = data && !isError && !isLoading ? data : address;
-
   return (
-    <button
-      className="h-8 text-green border-solid border-2 rounded-lg px-4 font-light max-w-[146px]"
-      onClick={() => (isConnected ? disconnect() : connect())}
-    >
-      {isConnected ? <div className="truncate">{ensName}</div> : 'Connect'}
-    </button>
+    <>
+      <ConnectButton
+        chainStatus="none"
+        showBalance={false}
+        accountStatus={{
+          smallScreen: 'avatar',
+          largeScreen: 'full',
+        }}
+      />
+    </>
   );
 };
 
